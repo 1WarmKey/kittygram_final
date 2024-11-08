842bba4571dee9f8793a90f3c41e23ce5e88d123
Баклушин Владислав <WarmKey@yandex.ru>
2024-11-08 21:48:09 +0300

    new ip

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','158.160.82.14']
+ALLOWED_HOSTS = ['localhost','kitgramfinal.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
